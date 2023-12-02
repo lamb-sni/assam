4cc4befe0ac2d2fe695be9a82943d0a9d35b5523
sni <>
2023-12-03 04:01:37 +0900

    create zero-one game view

--- cup/src/pages/zero-one/game/index.tsx
+++ cup/src/pages/zero-one/game/index.tsx
@@ -1,13 +1,110 @@
 import React from 'react'
-// import ClassNames from 'classnames'
+import ClassNames from 'classnames'
 import Button from '@/components/atoms/Button'
 import styles from './styles.module.css'
 
 const Game = () => {
   const [focusedMutiplier, setFocusedMultiplier] = React.useState('single')
+  const [scored, setScored] = React.useState<{ [key: 'first' | 'second' | 'third']: null | number }>({
+    first: null,
+    second: null,
+    third: null,
+  })
+
+  const getMultiplierValue = () => {
+    switch (focusedMutiplier) {
+      case 'single':
+        return 1
+      case 'double':
+        return 2
+      case 'triple':
+        return 3
+      default:
+        return 1
+    }
+  }
+
+  const setScoredState = (value: number) => {
+    const multiplier = getMultiplierValue()
+    const result = value === 50 ? 50 : value * multiplier
+
+    if (!scored.first) {
+      setScored({ ...scored, first: result })
+      return
+    }
+
+    if (!scored.second) {
+      setScored({ ...scored, second: result })
+      return
+    }
+
+    if (!scored.third) {
+      setScored({ ...scored, third: result })
+      return
+    }
+  }
 
   return (
     <main className={styles.game}>
+      <div className={styles.informations}>
+        <div className={styles.histories}>
+          <ul className={styles.history}>
+            <li className={styles.gameName}>301</li>
+            <li className={styles.item}>
+              <p className={styles.round}>1st</p>
+              <p className={styles.score}>60</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.round}>2nd</p>
+              <p className={styles.score}>35</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.round}>3rd</p>
+              <p className={styles.score}>72</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.round}>20th</p>
+              <p className={styles.score}>150</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.marks}>
+          <div className={styles.current}>256</div>
+
+          <ul className={styles.scores}>
+            {Object.values(scored).map((v, idx) => {
+              return (
+                <li className={styles.item} key={idx}>
+                  {v ? v : '-'}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className={styles.playersWrapper}>
+          <ul className={styles.players}>
+            <li className={styles.item}>
+              <p className={styles.name}>lamb</p>
+              <p className={styles.score}>240</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.name}>pita</p>
+              <p className={styles.score}>223</p>
+            </li>
+            <li className={ClassNames(styles.item, styles.current)}>
+              <p className={styles.name}>hi8</p>
+              <p className={styles.score}>256</p>
+            </li>
+            <li className={styles.item}>
+              <p className={styles.name}>lakeside</p>
+              <p className={styles.score}>219</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div className={styles.inputs}>
         <ul className={styles.multiplier}>
           <li className={styles.item}>
@@ -47,7 +144,7 @@ const Game = () => {
               <li className={styles.item} key={idx}>
                 <Button
                   onClick={() => {
-                    console.log(idx + 1)
+                    setScoredState(idx + 1)
                   }}
                   label={(idx + 1).toString()}
                 />
@@ -57,7 +154,7 @@ const Game = () => {
           <li className={styles.item}>
             <Button
               onClick={() => {
-                console.log('BULL')
+                setScoredState(50)
               }}
               label="BULL"
               isDisabled={focusedMutiplier === 'triple'}
@@ -66,7 +163,7 @@ const Game = () => {
           <li className={styles.item}>
             <Button
               onClick={() => {
-                console.log('OUT')
+                setScoredState(0)
               }}
               label="OUT"
             />
